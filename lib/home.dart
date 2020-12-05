1020baac80780ca76b93a4726d357ae80c293be2
arslanashraf006 <arslanashraf7864@gmail.com>
2020-12-05 14:22:05 +0500

    Commit:3 Add two level to run the app.

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red, title: Text('Dice App')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              color: Colors.red,
+              child: Text('Simple Level'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              color: Colors.red,
+              child: Text('Hard Level'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
